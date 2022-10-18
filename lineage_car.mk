b643619801d3eeb751dad05f8abd2dc8beba7671
Robert Schuh <github@eneticum.de>
2022-10-18 15:48:53 +0200

    Update lineage_car.mk

--- lineage_car.mk
+++ lineage_car.mk
@@ -6,3 +6,5 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.bluetooth.enablenewavrcp=false \
 
 PRODUCT_IS_AUTO := true
+
+$(call inherit-product, packages/services/Car/car_product/build/car.mk)
